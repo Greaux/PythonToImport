5718bc4c6566d377e14e7843acacd8fd7cc68024
Greaux <22226093+Greaux@users.noreply.github.com>
2019-12-26 21:06:02 +0300

    Update bruteparallel.py

--- lab3/bruteparallel.py
+++ lab3/bruteparallel.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-import threading
+from threading import Thread
 from zipfile import ZipFile
 import itertools
 
@@ -8,17 +8,28 @@ def decart(ls):
     return list(itertools.product(*ls)) 
 
 found = False
-with ZipFile('zzz.zip') as b:
-    for i in range(1, 5): 
-        sym = [chr(c) for c in range(ord('a'), ord('z') + 1)]
-        passw = map(lambda x: ''.join(x), decart([sym] * i))
-        for password in passw:
-            try:
-                b.extractall(pwd=password)
-                print 'Password: ' + password
-                found = True
-                break
-            except:
-                pass
-        if found:
-            print
+def brute(t,y):
+    with ZipFile('zzz.zip') as b:
+        for i in range(t, y): 
+            sym = [chr(c) for c in range(ord('a'), ord('z') + 1)]
+            passw = map(lambda x: ''.join(x), decart([sym] * i))
+            for password in passw:
+                try:
+                    b.extractall(pwd=password)
+                    print 'Password: ' + password
+                    found = True
+                    break
+                except:
+                    pass
+            if found:
+                print
+                thread1.join()
+                thread2.join()
+                thread3.join()
+thread1 = Thread(target=brute, args=(1,2))
+thread2 = Thread(target=brute, args=(3,4))
+thread3 = Thread(target=brute, args=(5,6))
+
+thread1.start()
+thread2.start()
+thread3.start()
