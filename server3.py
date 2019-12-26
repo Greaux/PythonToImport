4681256af1dfebc9402b03cc9130f7dba4957bc5
Greaux <22226093+Greaux@users.noreply.github.com>
2019-12-26 18:57:02 +0300

    Update server3.py

--- server3.py
+++ server3.py
@@ -1,18 +1,15 @@
-#!/usr/bin/python
 import socket
 import subprocess
 
-socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socket.bind(('127.0.0.1', 10001))
-socket.listen(1)
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind(('127.0.0.1', 10005))
+sock.listen(1)
 while True:
-    conn, _ = socket.accept()
-    while True:
-        data = conn.recv(1024)
-        if not data:
-            break
-        data = data.rstrip()
-        output = subprocess.check_output(data, shell=true)
-        conn.sendall(output)
-        conn.close
-        break
+	conn, _ = sock.accept()
+	data = conn.recv(1024)
+	if not data:
+		break
+	data = data.rstrip()
+	output= subprocess.check_output(data, shell=True)
+	conn.sendall(output)
+	conn.close()
